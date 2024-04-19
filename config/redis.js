cbcd24494d1486050699b3cbc4be7b403fc8712f
blackjac7 <febriansyah705@gmail.com>
2024-04-20 02:24:59 +0700

    build: deploy to GCP

--- config/redis.js
+++ config/redis.js
@@ -2,21 +2,21 @@ const { createClient } = require("redis");
 
 const client = async () => {
     // * RedisLabs
-    // const connection = createClient({
-    //     password: process.env.REDIS_PASSWORD,
-    //     socket: {
-    //         host: process.env.REDIS_HOST,
-    //         port: process.env.REDIS_PORT,
-    //     },
-    // });
-
-    // * Local Redis
     const connection = createClient({
+        password: process.env.REDIS_PASSWORD,
         socket: {
-            host: process.env.LOCAL_REDIS_HOST,
-            port: process.env.LOCAL_REDIS_PORT,
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
         },
     });
+
+    // * Local Redis
+    // const connection = createClient({
+    //     socket: {
+    //         host: process.env.LOCAL_REDIS_HOST,
+    //         port: process.env.LOCAL_REDIS_PORT,
+    //     },
+    // });
 
     await connection.connect();
 
