9cd0599763616b3bce108adf5225a2ec69f1dcad
blackjac7 <febriansyah705@gmail.com>
2024-05-02 20:11:55 +0700

    add: enable cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const routes = require("./routers");
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(
